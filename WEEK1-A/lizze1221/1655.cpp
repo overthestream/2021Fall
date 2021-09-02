9a5b760051761d1a903813b1163d502d6b9b8670
Hoeun Lee <hoeunlee228@gmail.com>
2021-09-02 19:04:04 +0900

    Update 1655.cpp
    COUT 사용하시려면 `cout.tie(NULL);` 도 사용해주시는것이 좋아요!

--- WEEK1-A/lizze1221/1655.cpp
+++ WEEK1-A/lizze1221/1655.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
 int arr[10001];
@@ -9,6 +8,7 @@ priority_queue <int> L;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
     int n;
     cin >> n;
