410b8a4575603f16f04abfc6bcae79f5fb956a1f
EUNA-319 <81363864+EUNA-319@users.noreply.github.com>
2021-09-04 23:05:13 +0900

    EUNA-319 boj12605

---
+++ WEEK1-B/EUNA-319/boj17608.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int top=-1;
+int stack[100000];
+
+
+int main() {
+    int tc=0,num=0;
+    int count=0, pre=0, com=0;
+    cin>>tc;
+    cin.ignore();
+
+    for(int i=0;i<tc;i++){// 막대기의 개수만큼 입력받기
+        cin>>num;
+        cin.ignore();
+        stack[++top]=num;
+    }
+
+    com=stack[top--]; // 오른쪽 기준 가장 가까운 막대기의 높이 반환
+    count++;
+    while(top>-1){
+        pre = stack[top--];
+        if(com<pre) {// pre가 비교 숫자 com보다 높거나 같다면
+            count++; // 개수 증가
+            com = pre; // com update
+        }
+    }
+
+    cout<<count<<endl;
+    return 0;
+}
