60002e7ee7b814cbd7c7a2a7b33bf2f5b66d4a64
KJ <48792069+kwonjeong@users.noreply.github.com>
2021-09-07 11:10:25 +0900

    .

--- WEEK1-B/kwonjeong/boj11279.cpp
+++ WEEK1-B/kwonjeong/boj11279.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false); // 입출력 빠르게 해 줌
     cin.tie(NULL);
     cout.tie(NULL);
 
-    priority_queue<int> maxH;
-    int n, elem;
+    priority_queue<int> maxH; // 최대 힙 생성
+    int n, elem; // n: 연산의 개수 / elem: 원소
     cin >> n;
 
     for(int i = 0; i < n; i++){
-        cin >> elem;
-        maxH.push(elem);
-        if(elem == 0){
-            cout << maxH.top() << "\n";
-            maxH.pop();
+        cin >> elem; // 원소 입력받음
+        maxH.push(elem); // 입력받은 원소 push
+        if(elem == 0){ // 원소가 0이라면
+            cout << maxH.top() << "\n"; // 최대 힙의 top을 출력
+            maxH.pop(); // 출력했으므로 pop 해줌
         }
     }
 }
