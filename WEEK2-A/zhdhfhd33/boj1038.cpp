ad7fde4bc14de938f784f77e1e1f6e9b7fb8acc7
zhdhfhd33 <43158514+zhdhfhd33@users.noreply.github.com>
2021-09-14 18:11:55 +0900

    zhdhfhd33 boj1038
    성공

--- WEEK2-A/zhdhfhd33/boj1038.cpp
+++ WEEK2-A/zhdhfhd33/boj1038.cpp
@@ -1,13 +1,30 @@
+//성공
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
 using namespace std;
 
-int isDecreasing(const char* str) { //decreasing이면 -1을 반환. 아니면 i와 i+1이 다른 index를 반환
+int isDecreasing(long long a) { //decreasing이면 -1을 반환. 아니면 i와 i+1이 다른 index를 반환
+    char str[11];
+
+    sprintf(str, "%lld", a);
     for (int i = 0; i < strlen(str) - 1; ++i) {
         if (str[i] <= str[i + 1]) return i; //decreasing 아닐 때
     }
     return -1;//decreasing일 때
+}
+//각 숫작들이 정확히 1씩 차이나는지를 확인.
+int findIndex(long long a) {  //정확히 1씩 차이난다면 -1반환. 아니라면 인덱스 반환. 한자리수일 때는 0반환.
+    char str[11];
+    sprintf(str, "%lld", a);
+    const int LEN = strlen(str);
+    if (LEN == 1) {
+        return 0;
+    }
+    for (int i = LEN - 1; i > 0; --i) {
+        if (str[i - 1] != str[i] + 1) return i;
+    }
+    return -1;
 }
 
 void resetBuf(char* str) {
@@ -17,66 +34,81 @@ void resetBuf(char* str) {
 }
 
 int isLast(char* temp) {
-    for (int i = 0; i < strlen(temp)-1; ++i) {
+    for (int i = 0; i < strlen(temp) - 1; ++i) {
         if (temp[i] - 1 != temp[i + 1]) return 0;
     }
     return 1;
 }
 
+int get10Num(int a) {
+    int res = 1;
+    while (a--) {
+        res *= 10;
+    }
+    return res;
+}
+
 int main() {
     int N;
     scanf("%d", &N);
-
-    char buf[11] = { 0 }; //9876543210을 담기 위한 공간
-
-
     int count = 0;
     long long i = 0;
-    while ((long long)count <= N) {
+    long long answer =0;
+    for (; count <= N; ) {
+        if (i > 9876543210) {
+            printf("-1");
+            return 0;
+        }
+        int res = isDecreasing(i);
 
-        snprintf(buf, sizeof(buf), "%lld", i);
-        const int LEN = strlen(buf); //LEN = 자릿수
-        int res = isDecreasing(buf);
         if (res == -1) {
-            printf("decreasing[%d] : %lld\n", count, i);
+            //printf("decrease [%d]: %lld\n", count, i);
+            answer = i;
             ++count;
-
-            ++i; // i컨트롤
-
+            ++i;
         }
         else {
-            //printf("i : %d\n", i);
-            
 
-            int add = 1;
-            for (int j = 0; j < LEN - res - 1; ++j) {
-                add *= 10;
-            }
-            i += add;
-            char temp[11] = { 0 };
+            --i; //i 원상복구
+            char str[11];
+            sprintf(str, "%lld", i);
 
-            
-            snprintf(temp, sizeof(temp), "%lld", i);
-            if (res == 0) {
-
-                for (int j = strlen(temp) - 1, a = 0; j >= 1; --j, ++a) { //첫째자리만 빼고 모두 바꿈.
-                    temp[j] = 48 + a;
+            if (findIndex(i) == -1) { //각 자리 숫자가 1씩 차이날 때
+                
+                i += get10Num(strlen(str) - 1);
+                sprintf(str, "%lld", i);
+                if (str[0] == '1') { //자릿수가 바뀐 상황. ex) 98->100
+                    str[0] = '0' + strlen(str) - 1;
                 }
+                const int LEN = strlen(str);
+                for (int j = LEN - 1; j >= 1; --j) {
+                    str[j] = '0' + LEN - 1 - j;
+                }
+                sscanf(str, "%lld", &i);//i 컨트롤
+
+                //printf("1 : %d\n", i);
             }
-            //printf("before i : %lld\n", i);
-            temp[strlen(temp) - 1] = '0';
+            else {
+                int add = 1;
+                const int LEN= strlen(str);
+                for (int j = 0; j < LEN - res - 1; ++j) {
+                    add *= 10; 
+                }
+                i += add;
+                sprintf(str, "%lld", i);
 
-
-            sscanf(temp, "%lld\n", &i); //i컨트롤
-            //printf("after i : %lld\n", i);
-            if (i > 9876543210) {
-                printf("-1");
-                return 0;
+                //필요한 자릿수 만큼 더하고 그 밑은 전부 decreasing 중 가장 작은 값으로 채워야 한다.
+                for (int j = LEN - 1, a=0; j >= res + 1; --j, ++a) {
+                    str[j] = '0' + a;
+                    //printf("in for str : %s\n", str);
+                }
+                sscanf(str, "%lld", &i);
+                //printf("2 : %d\n", i);
             }
-
+            
         }
-
+        
     }
-    printf("%s", buf);
+    printf("%lld", answer);
     return 0;
 }
