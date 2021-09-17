a23bd8731828636f8c0bb02f5142246297072bd0
zhdhfhd33 <43158514+zhdhfhd33@users.noreply.github.com>
2021-09-18 01:05:26 +0900

    zhdhfhd33 boj2580
    성공

--- WEEK2-A/zhdhfhd33/boj2580.java
+++ WEEK2-A/zhdhfhd33/boj2580.java
@@ -1,6 +1,4 @@
-//쉬운 스도쿠는 푸는데 어려운 것은 empty stack err난다. -> 백트래킹이 (0,0)까지 진행되어서 풀 수 없다는 결론임.
-//모르겠다~
-
+//성공
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -20,17 +18,13 @@ class Triple {
 class Data {
 	int num;
 	int[] check; // 초기화 하지 않으면 자동 null대입.
-//	Data(int num, int [] check){
-//		this.num=num;
-//		this.check=check;
-//	}   
 
 	Data(int num) {
 		this.num = num;
 	}
 }
 
-public class Main {
+public class Test {
 	public static void printArr(Data arr[][]) {
 		for (int i = 0; i < arr.length; ++i) {
 			for (int j = 0; j < arr[0].length; ++j) {
@@ -40,17 +34,16 @@ public class Main {
 		}
 	}
 
-	
-
-	//전부 0으로 초기화
+	// 전부 0으로 초기화
 	public static void resetCheck(int check[]) {
-		for (int i=0; i<check.length; ++i) {
-			check[i]=0;
+		for (int i = 0; i < check.length; ++i) {
+			check[i] = 0;
 		}
 	}
+
 	// row, col을 한번에 검사가능.
 	public static void checkRowCol(Data arr[][], int row, int col) {
-		
+
 		for (int i = 0; i < arr.length; ++i) {
 			if (arr[row][i].num != 0) {
 				arr[row][col].check[arr[row][i].num] = 1;
@@ -61,12 +54,10 @@ public class Main {
 				arr[row][col].check[arr[i][col].num] = 1;
 			}
 		}
-
 	}
 
 	// 자신의 좌표를 전달하면 3*3 box안에서의 남은 수를 반환함.
 	public static void checkBox(Data arr[][], int row, int col) {
-//		int check[] = new int[10];
 		int startRow = row / 3 * 3;// *3해줘야한다.
 		int startCol = col / 3 * 3;
 		for (int i = startRow; i < startRow + 3; ++i) {
@@ -74,22 +65,22 @@ public class Main {
 				arr[row][col].check[arr[i][j].num] = 1; // 이미 사용된 수 라면 1을 대입.
 			}
 		}
-//		return check;
 	}
 
-	// 이렇게 몇개의 작은 함수로 나눠 작성하면 보기에는 쉽고 코드 짤 때는 쉬운데 check를 계속 만들고 합치고 해야하니 효율적인 방법은 아닌
-	// 듯.
+	// arr[row][col].check을 초기화 한 후 현재 상황에서 arr[row][col].num에 넣을 수 있는 수를
+	// arr[row][col].check에 저장.
 	public static void resetCheckAll(Data arr[][], int row, int col) {
 		try {
 			resetCheck(arr[row][col].check);
 			checkRowCol(arr, row, col);
 			checkBox(arr, row, col);
 		} catch (Exception e) {
-			
-		}
-		
 
+		}
 	}
+
+	// arr[row][col].check을 초기화 하지 않고 현재 상황에서 arr[row][col].num에 넣을 수 있는 수를
+	// arr[row][col].check에 저장.
 	public static void addCheckAll(Data arr[][], int row, int col) {
 		checkRowCol(arr, row, col);
 		checkBox(arr, row, col);
@@ -126,82 +117,52 @@ public class Main {
 		for (int i = 0; i < 3; ++i) {
 			for (int j = 0; j < 3; ++j) {
 				for (int k = 3 * i; k < 3 * i + 3;) {
-					for (int l = 3 * j; l < 3 * j + 3;) {		
+					for (int l = 3 * j; l < 3 * j + 3;) {
 						if (arr[k][l].num == 0) { // 빈칸일 때
 							addCheckAll(arr, k, l);
 							int cnt = getCount(arr[k][l].check);
 							if (cnt == 0) {// 앞에서 잘못되었을 때 stack에서 이전 좌표를 꺼낸다.
-								Triple before = st.peek();
+								resetCheck(arr[k][l].check);// 고생함
+								Triple before = st.peek();// 이놈 때문에 고생좀 했다.
 								st.pop();
 								k = before.x;
 								l = before.y;
 								arr[k][l].num = 0;// 0으로 만들어줘야함.
-								while (getCount(arr[k][l].check) == 0) {
-									arr[k][l].check[before.idx] = 0;// check도 다시 설정.
-									before=st.peek();
+								while (getCount(arr[k][l].check) == 0) {// stack에서 꺼낸 좌표도 더이상 선택지가 없을 수 있기 때문에 선택지가 나타날
+																		// 때 까지 stack에서 pop()
+									resetCheck(arr[k][l].check);// 한번 이상 stack을 꺼낼 때만 resetCheck()해야한다.
+
+									arr[k][l].num = 0;// num도 다시 0으로
+									before = st.peek();
 									st.pop();
-									
-									k=before.x;
-									l=before.y;
-									arr[k][l].num=0;
-									i=k/3;
-									j=l/3;
-									if (!(l< 3 * j + 3)) {
-										--k;
-									}
+									k = before.x;
+									l = before.y;
+									i = k / 3;
+									j = l / 3;
 								}
-								continue; // 뒤로 가는 것이기 때문에 st.push()없다.
-							} else {//cnt != 0일 때
+								arr[k][l].num = 0;// while에서 구한 새로운 k,l에 대해서도 해줘야함. - 마지막에 찾은 허점 ㅎㅎ...
+
+								continue; // 변수 l에 대한 continue. l++안한다. 뒤로 가는 것이기 때문에 st.push()없다.
+							} else {// cnt != 0일 때
 								for (int t = 1; t < arr[k][l].check.length; ++t) {
 									if (arr[k][l].check[t] == 0) {
 										arr[k][l].num = t;
 										arr[k][l].check[t] = 1;// 한번 사용을 했으면 지운다. stack에서 꺼내서 다시 이 좌표로 왔을 때 다음 숫자를 선택해야
 																// 하니까
+
+										// 아래는 디버깅할 때 사용한 출력문.
 //										System.out.printf("arr[%d][%d] : %d\n", k, l, arr[k][l].num);
 //										printArr(arr);
 //										System.out.println();
 										st.push(new Triple(k, l, t));
-
-										
-										
-										//다음 위치에 대한 check배열을 갱신.
-										
-										if (l+1 < 3 * j + 3) {
-											resetCheckAll(arr, k, l+1);
-										}
-										else if (k+1 < 3  * i + 3){
-											resetCheckAll(arr, k+1, 3*j);
-										}
-										else if (j+1 < 3){
-											resetCheckAll(arr, 3*i, 3*(j+1));
-										}
-										else if (3*(i+1)<9 && 3*(j+1)<9){
-											resetCheckAll(arr, 3*(i+1), 3*(j+1));
-										}
-										
-										
 										break;
-									}//if end
+									} // if end
 								}
 
 							}
 
 							// else end
 
-						}
-						else {
-							if (l+1 < 3 * j + 3) {
-								resetCheckAll(arr, k, l+1);
-							}
-							else if (k+1 < 3  * i + 3){
-								resetCheckAll(arr, k+1, 3*j);
-							}
-							else if (j+1 < 3){
-								resetCheckAll(arr, 3*i, 3*(j+1));
-							}
-							else if (3*(i+1)<9 && 3*(j+1)<9){
-								resetCheckAll(arr, 3*(i+1), 3*(j+1));
-							}
 						}
 
 						++l;
