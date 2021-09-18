67c9de4ae1dd569978a2a0f5094451de3847105a
leaf482 <css64534@gmail.com>
2021-09-18 01:32:03 -0700

    leaf482

---
+++ WEEK2-B/leaf482/boj2959.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class boj2959 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[4];
+		
+		for (int i = 0; i < 4; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		System.out.println(arr[0] * arr[2]);
+	}
+}
